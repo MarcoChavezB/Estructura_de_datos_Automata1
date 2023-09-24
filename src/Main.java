912ed4fac6469d4ce22f2cdbc08c4f5d5dc7378d
Marco <marco1102004@gmail.com>
2023-09-24 10:21:51 -0600

    arreglos agregados

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
         String input;
         Boolean correctInput = false;
-        char[] abecedarioEspanol = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-        char[] vocalesEspanol = {'A', 'E', 'I', 'O', 'U'};
-
+        char[] letrasMayus = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        char[] vocalesMayus = {'A', 'E', 'I', 'O', 'U'};
+        char[] consonantes = {'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'};
+        int[] digitos = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] digitosPares = {0, 2, 4, 6, 8};
+        int[] digitosImpares = {1, 3, 5, 7, 9};
     }
 }
