fc1a663db5f1c3a89e19df6773b02ebe152ca063
Marco <marco1102004@gmail.com>
2023-09-24 10:16:35 -0600

    commit inicial

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        String input;
+        Boolean correctInput = false;
+        char[] abecedarioEspanol = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        char[] vocalesEspanol = {'A', 'E', 'I', 'O', 'U'};
+
+    }
+}
